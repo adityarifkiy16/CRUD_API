8e38eb5696d88234c5df603af9dedefdd8187c75
adityarifkiy16 <adityarifkistemba@gmail.com>
2024-12-02 10:14:41 +0700

    refactor : change type field jadwal makul migration

--- database/migrations/2024_11_29_140500_create_makul_table.php
+++ database/migrations/2024_11_29_140500_create_makul_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('makul', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->dateTime('jadwal');
+            $table->time('jadwal');
             $table->timestamps();
         });
     }
