ce804e5a00b0b2665e6f763c34614e2808148704
adityarifkiy16 <adityarifkistemba@gmail.com>
2024-12-02 10:14:11 +0700

    fix: route

--- routes/api.php
+++ routes/api.php
@@ -14,27 +14,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::post('/register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
 Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+
 Route::group(['middleware' => ['auth:sanctum']], function () {
     // Mahasiswa
-    Route::get('/mahasiswa/create', [\App\Http\Controllers\Api\MahasiswaController::class, 'store']);
+    Route::post('/mahasiswa/create', [\App\Http\Controllers\Api\MahasiswaController::class, 'store']);
     Route::get('/mahasiswa/read', [\App\Http\Controllers\Api\MahasiswaController::class, 'index']);
-    Route::put('/mahasiswa/update', [\App\Http\Controllers\Api\MahasiswaController::class, 'update']);
-    Route::delete('/mahasiswa/delete', [\App\Http\Controllers\Api\MahasiswaController::class, 'destroy']);
+    Route::put('/mahasiswa/update/{id}', [\App\Http\Controllers\Api\MahasiswaController::class, 'update']);
+    Route::delete('/mahasiswa/delete/{id}', [\App\Http\Controllers\Api\MahasiswaController::class, 'destroy']);
     // Dosen
-    Route::get('/dosen/create', [\App\Http\Controllers\Api\DosenController::class, 'store']);
+    Route::post('/dosen/create', [\App\Http\Controllers\Api\DosenController::class, 'store']);
     Route::get('/dosen/read', [\App\Http\Controllers\Api\DosenController::class, 'index']);
-    Route::put('/dosen/update', [\App\Http\Controllers\Api\DosenController::class, 'update']);
-    Route::delete('/dosen/delete', [\App\Http\Controllers\Api\DosenController::class, 'destroy']);
+    Route::put('/dosen/update/{id}', [\App\Http\Controllers\Api\DosenController::class, 'update']);
+    Route::delete('/dosen/delete/{id}', [\App\Http\Controllers\Api\DosenController::class, 'destroy']);
     //Makul
-    Route::get('/makul/create', [\App\Http\Controllers\Api\MakulController::class, 'store']);
+    Route::post('/makul/create', [\App\Http\Controllers\Api\MakulController::class, 'store']);
     Route::get('/makul/read', [\App\Http\Controllers\Api\MakulController::class, 'index']);
-    Route::put('/makul/update', [\App\Http\Controllers\Api\MakulController::class, 'update']);
-    Route::delete('/makul/delete', [\App\Http\Controllers\Api\MakulController::class, 'destroy']);
+    Route::put('/makul/update/{id}', [\App\Http\Controllers\Api\MakulController::class, 'update']);
+    Route::delete('/makul/delete/{id}', [\App\Http\Controllers\Api\MakulController::class, 'destroy']);
 
-
-    Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
+    Route::get('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
